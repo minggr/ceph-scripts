563e88fcff1e629ba2bed1d786b33135e5560fc2
Ming Lin <minggr@gmail.com>
2017-02-01 15:00:58 -0800

    build.sh: disable optimization

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 mkdir $HOME/ceph-install
 cd ceph
-CXXFLAGS=-ggdb ./configure --prefix=$HOME/ceph-install --with-debug
+CXXFLAGS="-ggdb -g -O0" CFLAGS="-ggdb -g -O0" ./configure --prefix=$HOME/ceph-install --with-debug
 make -j `getconf _NPROCESSORS_ONLN` && make install
+
+#master branch
+cmake -DCMAKE_BUILD_TYPE=Debug ..
